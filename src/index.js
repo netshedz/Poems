1b8108cea494bbedd3a03d6f8a85c80dad3acec0
netshedzo <67120128+netshedz@users.noreply.github.com>
2024-07-31 00:44:55 +0200

    Added API intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function PoemGenerator(event) {
-  event.preventDefault();
-
+function displayPoems(response) {
   new Typewriter("#generate-poems", {
-    strings: "How do I love thee? Let me count the ways",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function PoemGenerator(event) {
+  event.preventDefault();
+
+  let input = document.querySelector("#input-title");
+  let apiKey = "2f93ebcd2o4334a22af8dt6e0784500a";
+  let context =
+    " i love to write short poem that soothes hearts and provides a sense of calm and hope and feel love.Your mission is to generate five line poem in basic html. Make sure to follow the user instructions.Make sure to always include the title of the poem and the title should be bold and black. always separate the poem with a br.Please make sure to include the signature at the bottom of the poem and the signature should be inside <strong>element:Dzhivhuho Netshedzo, at the end of the poem";
+  let prompt = `user instructions:generate a poem ${input.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoems);
 }
 
 let formElement = document.querySelector("#poem-generator");
