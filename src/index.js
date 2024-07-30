cba1984a760a06515cd7a408faa41e8720b97c76
netshedzo <67120128+netshedz@users.noreply.github.com>
2024-07-30 23:49:08 +0200

    added the basic form handling and the typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function PoemGenerator(event) {
+  event.preventDefault();
 
+  new Typewriter("#generate-poems", {
+    strings: "How do I love thee? Let me count the ways",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#poem-generator");
+formElement.addEventListener("submit", PoemGenerator);
